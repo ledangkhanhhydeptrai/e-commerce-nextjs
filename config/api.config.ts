a5f2396081c51b9e42e4676555fa6ffc2bf83e41
khanhhy <ledangkhanhhy@gmail.com>
2025-12-30 12:41:06 +0700

    fix(api): correct baseURL assignment and log API URL for debugging

--- config/api.config.ts
+++ config/api.config.ts
@@ -4,10 +4,10 @@ const baseURL =
   process.env.NODE_ENV === "production"
     ? "" // gọi /api/... của Next.js
     : process.env.NEXT_PUBLIC_API_URL; // localhost
-
+console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
 const API = axios.create({
-  baseURL,
-  timeout: 15000,
+  baseURL: "http://localhost:8080",
+  timeout: 15000
 });
 
 export default API;
